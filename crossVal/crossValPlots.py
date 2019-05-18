bb27b1488cad81422bebf0e596d229bf7fe769b0
Christian <Nadell@Christians-MacBook-Pro-832.local>
2019-05-18 19:17:41 -0400

    Added interpolation to plot.

--- crossVal/crossValPlots.py
+++ crossVal/crossValPlots.py
@@ -6,6 +6,7 @@ from matplotlib.patches import Rectangle
 import seaborn as sns
 
 #enable seaborn defaults for matplotlib
+SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
 sns.set()
 
 import matplotlib
@@ -37,6 +38,43 @@ def plotErrorGrad(path, proj_func='mean'):
     # calculate MSE between the true labels and the predictions
     MSEs = [np.mean(np.square(truth-pred)) for truth, pred in zip(truths, preds)]
 
+    def plotProjSingle(indices, y_lims, title_val, vline=49):
+        fig, ax = plt.subplots()
+        labels = ['${}_{}$'.format(title_val, i + 1) for i in range(4)]
+        for cnt, index in enumerate(range(indices[0], indices[1])):
+            unique_vals = sorted(set(feats[:, index]))
+            avg_h_MSEs = []
+            mse_std_devs = []
+            for unique_val in unique_vals:
+                mse_holder = []
+                for val, mse in zip(feats[:, index], MSEs):
+                    if val == unique_val:
+                        mse_holder.append(mse)
+                avg_h_MSEs.append(np.mean(mse_holder))
+                mse_std_devs.append(np.std(mse_holder))
+            ax.set_aspect(1.0/ax.get_data_ratio()*1.6)
+            ax.set_ylim(y_lims)
+            #print('plt is {} index {}, num unique vals is {}'.format(title_val, index, len(unique_vals)))
+            xticksInt = [int(x) for x in np.arange(min(list(unique_vals)),
+                                 max(list(unique_vals)) + 1,
+                                 (max(list(unique_vals)) - min(list(unique_vals)))/3)]
+            plt.xticks(xticksInt)
+            colors = [(.27, .004, .33), (.2, .39, .55), (.155, .68, .50), (.84, .89, .102)]
+            ax.plot(list(unique_vals), avg_h_MSEs, marker='o', label=labels[cnt], color=colors[cnt])
+            plt.fill_between(list(unique_vals),
+                             (np.array(avg_h_MSEs) - .3*np.array(mse_std_devs)),
+                             (np.array(avg_h_MSEs) + .3*np.array(mse_std_devs)),
+                             color=colors[cnt],
+                             alpha=.3
+                             )
+            ax.legend(loc='lower right', bbox_to_anchor=(.3, .65, .1, .05))
+            plt.axvline(x=vline, linestyle='--')
+            plt.savefig('singProjs_{}_{}.png'.format(title_val, dir))
+            #fig.suptitle("average MSE vs {}".format(title_val))
+
+    plotProjSingle(indices=[0, 4], y_lims=[0, .007], title_val='h', vline=47)
+    plotProjSingle(indices=[4, 8], y_lims=[0, .007], title_val='r', vline=49)
+
     # project features onto 2D space
     # makes assumptions about which columns should be mapped together
     # will need to be changed based on how input data is organized
@@ -64,20 +102,23 @@ def plotErrorGrad(path, proj_func='mean'):
         mse_agg = []
         for pnt in unique_pnts:
             mse_holder = []
+            mse_holder = []
             for feat, mse in zip(feats_prj, MSEs):
-                # if the projected input vectors (here geometries) are the same, then grab all the mse values
-                # so that we can average later
                 if np.array_equal(feat, pnt):
                     mse_holder.append(mse)
-            # calculate mean
             mse_agg.append(np.mean(mse_holder))
 
         f, ax = plt.subplots()
-        # normalize the error values to the maximum and print
         colors = [perc for perc in mse_agg/max(mse_agg)]
         print('Projection is with max function, MSE normalized to {}'.format(max(mse_agg)))
-        # plot with nice plasma color
-        scatter = ax.scatter(x=unique_pnts[:, 0], y=unique_pnts[:, 1], c=colors, cmap='plasma')
+        #rearrange data for contour plotting
+        cont_data = [[r, h, mse] for r, h, mse in zip(unique_pnts[:, 0], unique_pnts[:, 1], colors)]
+        cont_data.sort(key=lambda x: x[2])
+        cont_data = np.array(cont_data)
+        cont = ax.tricontourf(cont_data[:, 0], cont_data[:, 1], cont_data[:, 2], 20 , cmap='plasma')
+        # scatter plot on top
+        scatter = ax.scatter(x=unique_pnts[:, 0], y=unique_pnts[:, 1], edgecolors='gray',
+                             linewidths=2, c=colors, facecolors='none', cmap='plasma')
         f.colorbar(scatter)
 
     # same thing here with min. Could eliminate some of this redundant code later
@@ -117,7 +158,7 @@ if __name__=="__main__":
     # specify list of directories to pull and plot data from
     dirs = ['20190410_142248']
     for dir in dirs:
-        plotErrorGrad(dir, 'mean')
+        plotErrorGrad(dir, 'max')
 
     #show the plots generated
     plt.show()
